388a46b79061b87979e60a21b8cb59ae5c68dc4b
SSeed1 <46745943+SSeed1@users.noreply.github.com>
2020-10-31 15:52:18 +0200

    Final Version

--- lab_4/js/post.js
+++ lab_4/js/post.js
@@ -1,68 +1,78 @@
 "use strict";
-let posts = [
-     {
-     id : 1 ,
-     title : 'Docker for Rails' ,
-     created_date : '2018-11-03'
-     } ,
-     {
-     id : 2 ,
-     title : 'HTML5 for Beginners' ,
-     created_date : '2019-01-11'
-     } ,
-     {
-     id : 3 ,
-     title : 'CSS3 for dummies' ,
-     created_date : '2018-05-18'
-     } ,
-     {
-     id : 4 ,
-     title : 'jQuery Master' ,
-     created_date : '2019-10-23'
-     }
-     ]
-
-     function createPostsRows(posts){
-         $('#posts').html('')
-         let rowsHtml=''
-         posts.forEach(function(posts){
-             rowsHtml+= 
-             `<tr><td>${posts.id}</td><td>${posts.title}</td><td>${posts.created_date}</td></tr>`
-         })
-         $('#posts').html(rowsHtml)
-     }
-     createPostsRows(posts)
-
-    $('#sort-button').click(function(event){
-    if($('#SortingValue').val()=='id'){
-         SortById(posts)
+let posts = [{
+        id: 1,
+        title: 'Docker for Rails',
+        created_date: '2018-11-03'
+    },
+    {
+        id: 2,
+        title: 'HTML5 for Beginners',
+        created_date: '2019-01-11'
+    },
+    {
+        id: 3,
+        title: 'CSS3 for dummies',
+        created_date: '2018-05-18'
+    },
+    {
+        id: 4,
+        title: 'jQuery Master',
+        created_date: '2019-10-23'
     }
-    if($('#SortingValue').val()=='title'){
+]
+
+function createPostsRows(posts) {
+    $('#posts').html('')
+    let rowsHtml = ''
+    posts.forEach(function (posts) {
+        rowsHtml +=
+            `<tr><td>${posts.id}</td><td>${posts.title}</td><td>${posts.created_date}</td></tr>`
+    })
+    $('#posts').html(rowsHtml)
+}
+createPostsRows(posts)
+
+$('#sort-button').click(function (event) {
+    if ($('#SortingValue').val() == 'id') {
+        SortById(posts)
+    }
+    if ($('#SortingValue').val() == 'titile') {
         SortByTitle(posts)
     }
+    if($('#SortingValue').val()=='date'){
+        SortByDate(posts)
+    }
+})
+
+function SortById(posts) {
+    posts = posts.sort(function (a, b) {
+        if (a['id'] > b['id']) {
+            return 1
+        } else {
+            return -1
+        }
     })
- 
-    function SortById(posts){
-        posts=posts.sort(function(a,b){
-            if(a['id']>b['id']){
-                return 1
+    createPostsRows(posts);
+}
 
-            }
-            else{
-                return -1
-            }
-        })
-        createPostsRows(posts);
-    }
+function SortByTitle(posts) {
+    posts = posts.sort(function (a, b) {
+        if (a['title'] > b['title']) {
+            return 1
+        } else {
+            return -1
+        }
+    })
+    createPostsRows(posts);
+}
 
-    function SortByTitle(posts){
-        posts=posts.sort(function(a,b){
-            if(a['title']>b['title']){
-                return 1
-            }
-            else{
-                return -1
-            }
-       })
-       createPostsRows(posts);
-    }
+function SortByDate(posts) {
+    posts = posts.sort(function (a, b) {
+        if (a['created_date'] > b['created_date']) {
+            return 1
+        } else {
+            return -1
+        }
+    })
+    createPostsRows(posts);
+}
